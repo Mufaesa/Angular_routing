5ca622b049371f34cdfaa59ca71b8f3adfa236d4
tom.vrijmoet <tom.vrijmoet@ns.nl>
2023-11-09 17:57:14 +0100

    Init

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  public servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    // whereas the routerLink in the html file always knows which route is active, the Router in your component does not
+    // To give this knowledge to the component, importing and using "ActivatedRoute" is required 
+    this.router.navigate(['servers'], {relativeTo: this.route})
+  }
+
+}
