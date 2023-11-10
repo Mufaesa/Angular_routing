8bef7132cbbd64d30af70f7939e982b57cbdc5ba
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2023-11-10 18:45:38 +0100

    Added dynamic routing based on ID

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
+// Snapshots are a single instance of the url when initialized
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+// Subscribing to the params of the url will update the user object whenever a change on the parameters has been made
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.user.id = params ['id'];
+        this.user.name = params ['name'];
+      } 
+    );
   }
 
 }
