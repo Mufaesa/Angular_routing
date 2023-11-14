39762d9a6c7e1ad40d2d962142e4913fda688c03
tom.vrijmoet <tom.vrijmoet@ns.nl>
2023-11-14 11:34:35 +0100

    Child authentication toegevoegd

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs-compat";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(private authService: AuthService, private router: Router) {}
 
+    // canActivate is used by the routing in app-routing.module.ts
+    // It MUST be called canActivate because it is Angular functionality
+    // Works with a promise that is sent from auth.service.ts
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
         return this.authService.isAuthenticated()
@@ -19,5 +22,10 @@ export class AuthGuard implements CanActivate {
                     }
                 }
             )
+    }
+
+    canActivateChild(childRoute: ActivatedRouteSnapshot, 
+                    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
+        return this.canActivate(childRoute, state);
     }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,8 @@
 export class AuthService {
     loggedIn = false;
 
+    // Hardcoded authentication method
+    // Returns a promise with a timout on it.
     isAuthenticated(){
         const promise = new Promise(
             (resolve, reject) => {
