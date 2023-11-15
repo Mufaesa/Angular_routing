8c71e9bb01eb010bfc05c7b6b9d684f5316c089c
tom.vrijmoet <tom.vrijmoet@ns.nl>
2023-11-15 16:40:11 +0100

    Added a resolver to fetch servers

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,17 +16,24 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
+    this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server'];
+        }
+      );
+
     // Adding the '+' in front of 'this' turns the returned id into a number
     // Instead of '1', it now returns the number 1
-    const serverId = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(serverId);
+    // const serverId = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(serverId);
 
-    this.route.params
-    .subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-      } 
-    );
+    // this.route.params
+    // .subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   } 
+    // );
   }
 
   onEdit(){
